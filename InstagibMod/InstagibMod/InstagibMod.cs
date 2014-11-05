fa9de25c877bc84ee97a3932353c7bae59da4486
Isaac Brodsky <ibrodsky@plazsoft.com>
2014-11-05 14:49:29 -0600

    Only change damage on player ship's weapons.

--- InstagibMod/InstagibMod/InstagibMod.cs
+++ InstagibMod/InstagibMod/InstagibMod.cs
@@ -9,40 +9,76 @@ using Yargis;
 
 namespace InstagibMod
 {
-    public class InstagibMod : ISessionMod
+    public class InstagibMod : SessionMod
     {
-        public string Name
+        public override string Name
         {
             get { return "Instagib"; }
         }
+        public override string Description
+        {
+            get { return "Weapons instantly destroy their targets.";  }
+        }
 
-        public void SessionDraw()
+        public override void SessionDraw()
         {
 
         }
 
-        public void SessionEnd()
+        public override void SessionEnd()
         {
 
         }
 
-        public void SessionEndRound()
+        public override void SessionEndRound()
         {
 
         }
 
-        public void SessionStart()
+        public override void SessionStart()
         {
-            //Make all damage deal maximum damage
-            Global.Session.Level._Rules.DamageRatio = float.MaxValue;
+            //Uncomment and change to adjust the damage
+            //done by everything in the current game session.
+            //Global.Session.Level._Rules.DamageRatio = 1f;
         }
 
-        public void SessionStartRound()
+        public override void SessionStartRound()
         {
 
         }
 
-        public void SessionUpdate()
+        public override void SessionUpdate()
+        {
+
+        }
+
+        public override void SessionPlayerSpawn(Player p, Entity ship)
+        {
+            //For each weapon on the ship
+            for (int i = 0; i < ship.Objects.Count; i++)
+            {
+                //Obtain the weapon fire component if one exists.
+                var weaponFire = ship.Objects[i].GetComp<Yargis.Components.WeaponFire>();
+                if (weaponFire != null)
+                {
+                    //Retrieve the damage component from the bullet
+                    //it fires.
+                    var bulletDamage = weaponFire.Bullet.GetComp<Yargis.Components.DamageComponent>();
+                    if (bulletDamage != null)
+                    {
+                        //Make the weapon do maximum damage (only if the
+                        // weapon would have done damage.)
+                        if (bulletDamage.Value > 0f)
+                        { 
+                            bulletDamage.Value = float.MaxValue;
+                        }
+                    }
+                }
+            }
+        }
+
+        public override bool SessionCanConfigure { get { return false; } }
+        public override void SessionConfigure() 
         {
 
         }
